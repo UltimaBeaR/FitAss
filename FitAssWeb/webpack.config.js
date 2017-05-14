3f5f736ff4913e0758f62e07943bf1d04e44fcc8
UltimaBeaR <thebear@yandex.ru>
2017-05-14 09:36:37 +0300

    Webpack, Webpack clear plugin, Typescript (+loader) added as npm dev dependencies. Also added npm task runner that runs webpack script (from webpack.config.js) before every build. NPM Task Runner (visual studio extension) must be installed to use it. Project file changed to disable default typescript transpiling (Also for excluding node_modules folder, npm-debug.log and making all .ts files to be considered as content) Added .gitignore on project level to exclude generated bundles from git repository.

---
+++ FitAssWeb/webpack.config.js
@@ -0,0 +1,41 @@
+﻿"use strict"
+{
+    // Требуется для формирования полного output пути
+    let path = require('path');
+
+    // Плагин для очистки выходной папки (bundles) перед созданием новой
+    const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+    // Путь к выходной папке
+    const bundlesFolder = "wwwroot/bundles/";
+
+    module.exports = {
+        // Точка входа в приложение
+        entry: "./Scripts/main.ts",
+
+        // Выходной файл
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, bundlesFolder)
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        plugins: [
+            new CleanWebpackPlugin([bundlesFolder])
+        ],
+
+        // Включаем генерацию отладочной информации внутри выходного файла (Нужно для работы отладки клиентских скриптов)
+        devtool: "inline-source-map"
+    };
+}
